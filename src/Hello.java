5118e9ca54dbcd8471c3fd9e66e3bfcfac8a3567
willschmidt <willschmidt@wills-air-5.unl.edu>
2022-08-25 18:52:51 -0500

    Added major

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Will Schmidt");
+		System.out.println("I'm a computer science major");
 	}
 	
 }
