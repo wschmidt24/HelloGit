7997246d86dead00b13332e0d78b54f354273187
andrewfoster <andrewfoster@andrews-mbp-7.unl.edu>
2022-08-26 15:33:18 -0500

    Added my name and major.

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Will Schmidt");
 		System.out.println("I'm a computer science major");
+		
+		System.out.println("My name is Andy Foster");
+		System.out.println("I'm also a computer science major!");
 	}
 	
 }
